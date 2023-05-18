ce2cbcf9f49f67a228ddc9ec911c2facc14ffcc0
BiancaCriste <criste.bianca@gmail.com>
2023-05-18 22:16:37 +0300

    Create Model and Service for Bike Repair

---
+++ webapp/src/app/bike-repair.model.ts
@@ -0,0 +1,10 @@
+export interface BikeRepair {
+  id: number;
+  description: string;
+  startDate: string;
+  dueDate: string;
+  clientName: string;
+  clientPhone: number;
+  status: string;
+  price: number;
+}
