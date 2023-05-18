d182bb1fdddcee927b6adc12b34b33d147e93317
BiancaCriste <criste.bianca@gmail.com>
2023-05-18 22:17:10 +0300

    Add Bike Repair Component to view.

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BikeRepairListComponent } from './bike-repair-list/bike-repair-list.component';
+import {BikeRepairService} from "./bike-repair.service";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BikeRepairListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BikeRepairService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
