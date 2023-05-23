fa7b86caf1e1572e58a70648ae65b8c867226cba
BiancaCriste <criste.bianca@gmail.com>
2023-05-23 21:22:17 +0300

    Populate list repair component and add it to parent component

--- webapp/src/app/repairs/list-repairs/list-repairs.component.ts
+++ webapp/src/app/repairs/list-repairs/list-repairs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {BikeRepair} from "../../_model/bike-repair.model";
+import {BikeRepairService} from "../../services/bike-repair.service";
 
 @Component({
   selector: 'app-list-repairs',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-repairs.component.css']
 })
 export class ListRepairsComponent implements OnInit {
+  repairs: BikeRepair[] = [];
 
-  constructor() { }
+  constructor(private bikeService: BikeRepairService) { }
 
   ngOnInit(): void {
+    this.bikeService.getBikeRepair()
+      .subscribe((repairs: BikeRepair[]) => this.repairs = repairs);
   }
 
 }
