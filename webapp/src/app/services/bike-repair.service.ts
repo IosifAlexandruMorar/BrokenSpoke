8937dc8c865838ecbf59137165be87b2e88c1fdd
BiancaCriste <criste.bianca@gmail.com>
2023-05-23 21:18:43 +0300

    Create bike repair model and service

---
+++ webapp/src/app/services/bike-repair.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {BikeRepair} from "../_model/bike-repair.model";
+
+@Injectable()
+export class BikeRepairService {
+  private bikeRepairget = 'http://localhost:8080/api/v1/bikerepair';
+  private bikeRepairURL = 'http://localhost:8080/api/v1';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getBikeRepair(): Observable<BikeRepair[]> {
+    return this.httpClient.get<Array<BikeRepair>>(this.bikeRepairget);
+  }
+}
