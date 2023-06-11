d6a37be2552a7e89362a90183b2f645cfcbf6869
BiancaCriste <criste.bianca@gmail.com>
2023-06-11 16:07:58 +0300

    Add sign up form

--- webapp/src/app/signup/signup.component.ts
+++ webapp/src/app/signup/signup.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { UserService } from "../services/user.service";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
+  signupForm = this.formBuilder.group({
+    userName: ['', Validators.required],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
 
-  ngOnInit(): void {
+  ngOnInit() { }
+
+  onSubmit():void {
+    console.log(this.signupForm.value);
+    if (this.signupForm.invalid) {
+      return;
+    }
+
+    const formData = this.signupForm.value;
+    this.userService.signUpUser(formData).subscribe(
+      response => {
+        console.log('Sign up successful:', response);
+        this.signupForm.reset();
+      },
+      error => {
+        console.error('Sign-up failed: ', error);
+      }
+    );
   }
-
 }
